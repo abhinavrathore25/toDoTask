097e7c30547d08507d8a72767197ac6ba757f940
abhin <abhinavrathore.ar@gmail.com>
2022-10-21 02:37:38 +0530

    Final Commit

---
+++ build/precache-manifest.4d763b49a6ea34fb2ed28280b253d4bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86d5839bb474c84003668ca907e6a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bbe94d9aab3969a360",
+    "url": "/static/js/2.6ee16f53.chunk.js"
+  },
+  {
+    "revision": "364f6922c68350b27ed7",
+    "url": "/static/js/main.40a31e2e.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
