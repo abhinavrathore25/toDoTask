107e37f4dc0cf2a7e306df453a39ddfa7f75d719
Bagish Pandey <bagish.pandey@ad.infosys.com>
2023-05-22 02:02:19 +0530

    hi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,8 @@ function App() {
 
   //Abhinav
   const [notes, setNotes] = useState([]);
-<<<<<<< HEAD
   const [silky, setSilky] = useState(''); 
    const [selfBagish,setSelfBagish]=useState('');
-=======
-  const [silky, setsilky] = useState('');
->>>>>>> f26e377 (silky's changes)
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem('data'));
     if (data != null) {
