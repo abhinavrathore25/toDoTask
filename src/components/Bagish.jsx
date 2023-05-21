865ad5b5d77f06f13447a678c8c66d70f8730526
Bagish Pandey <bagish.pandey@ad.infosys.com>
2023-05-22 02:36:22 +0530

    done

---
+++ src/components/Bagish.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bagish = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bagish
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
+import Bagish from "./Bagish";
 
 function Header() {
   return (
     <header>
+      <Bagish/>
       <h1>
         <HighlightIcon />
         To-Do TASK
