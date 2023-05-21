fdc648a33938e38a73e9de63f388b75e919fe0a5
Silky <silkychourse00@gmail.com>
2023-05-22 01:03:47 +0530

    silky's commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   //Abhinav
   const [notes, setNotes] = useState([]);
-  const [selfBagish,setSelfBagish]=useState('');
+  const [silky, setSilky] = useState('');  const [selfBagish,setSelfBagish]=useState('');
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem('data'));
     if (data != null) {
